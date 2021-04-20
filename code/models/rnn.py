7bf4ea4de73ebf7ac118d4eeb77e7b22d0e82846
Juan Barajas <jbperches@gmail.com>
2021-04-19 23:04:37 -0500

    Added C-LSTM model

--- code/models/rnn.py
+++ code/models/rnn.py
@@ -13,19 +13,23 @@ def encode_text(input_text, MAX_SEQUENCE_LENGTH=50):
 
 
 def get_bidirectional_rnn(pretrained=False,
+                          embedding_matrix=None,
                           vocab_size=309467,
                           EMBEDDING_DIM=200,
                           MAX_SEQUENCE_LENGTH=50):
     if pretrained:
         model_rnn_simple = tf.keras.models.load_model("data/RNN_SIMPLE.h5")
     else:
-        embedding_matrix = load_from_pickle("data/embedding_matrix.pkl")
+        if embedding_matrix is None:
+            raise AttributeError("To load a non-pretrained model, an embedding_matrix is needed.")
 
         model_rnn_simple = tf.keras.Sequential([
-            tf.keras.layers.Embedding(vocab_size, EMBEDDING_DIM,
-                                      weights=[embedding_matrix],
-                                      input_length=MAX_SEQUENCE_LENGTH,
-                                      trainable=False),
+            tf.keras.layers.Embedding(
+                vocab_size, EMBEDDING_DIM,
+                weights=[embedding_matrix],
+                input_length=MAX_SEQUENCE_LENGTH,
+                trainable=False
+            ),
             tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
             tf.keras.layers.Dense(64, activation='relu'),
             tf.keras.layers.Dense(1, activation='sigmoid')
@@ -40,13 +44,57 @@ def get_bidirectional_rnn(pretrained=False,
     return model_rnn_simple
 
 
+def get_clstm(pretrained=False,
+              vocab_size=309467,
+              embedding_matrix=None,
+              EMBEDDING_DIM=200,
+              MAX_SEQUENCE_LENGTH=50):
+    if pretrained:
+        model_CLSTM = tf.keras.models.load_model("data/CLSTM.h5")
+    else:
+        if embedding_matrix is None:
+            raise AttributeError("To load a non-pretrained model, an embedding_matrix is needed.")
+
+        model_CLSTM = tf.keras.Sequential([
+            tf.keras.layers.Embedding(
+                vocab_size,
+                EMBEDDING_DIM,
+                weights=[embedding_matrix],
+                input_length=MAX_SEQUENCE_LENGTH,
+                trainable=False
+            ),
+            tf.keras.layers.SpatialDropout1D(0.2),
+            tf.keras.layers.Conv1D(64, 5, activation='relu'),
+            tf.keras.layers.Bidirectional(
+                tf.keras.layers.LSTM(64, dropout=0.2, recurrent_dropout=0.2)
+            ),
+            tf.keras.layers.Dense(64, activation='relu'),
+            tf.keras.layers.Dropout(0.5),
+            tf.keras.layers.Dense(1, activation='sigmoid')
+        ])
+
+        model_CLSTM.compile(
+            optimizer=tf.keras.optimizers.Adam(lr=0.01),
+            loss='binary_crossentropy',
+            metrics=['accuracy']
+        )
+
+    return model_CLSTM
+
+
 def inference(sentences):
     input_text = encode_text(sentences)
 
-    biderectional_rnn = get_bidirectional_rnn(pretrained=True)
-    biderectional_rnn_output = biderectional_rnn.predict(input_text)
+    embedding_matrix = load_from_pickle("data/embedding_matrix.pkl")
+
+    biderectional = get_bidirectional_rnn(pretrained=True, embedding_matrix=embedding_matrix)
+    clstm = get_clstm(pretrained=True, embedding_matrix=embedding_matrix)
+
+    biderectional_output = biderectional.predict(input_text)
+    clstm_output = clstm.predict(input_text)
 
     return {
-        'bidirectional_rnn': biderectional_rnn_output,
+        'bidirectional': biderectional_output,
+        'clstm': clstm_output,
     }
 
