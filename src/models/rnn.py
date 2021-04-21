16997da6ab5ac0a7c414a200572ebee9c66398b8
Juan Barajas <jbperches@gmail.com>
2021-04-20 19:28:46 -0500

    Changed the order of rnn inference to free memory

--- src/models/rnn.py
+++ src/models/rnn.py
@@ -14,85 +14,67 @@ def encode_text(input_text, MAX_SEQUENCE_LENGTH=50):
     return encoded_text
 
 
-def get_bidirectional_rnn(
-    pretrained=False,
-    embedding_matrix=None,
-    vocab_size=309467,
-    EMBEDDING_DIM=200,
-    MAX_SEQUENCE_LENGTH=50,
-):
+def get_bidirectional_rnn(pretrained=False,
+                          embedding_matrix=None,
+                          vocab_size=309467,
+                          EMBEDDING_DIM=200,
+                          MAX_SEQUENCE_LENGTH=50):
+
     if pretrained:
         model_rnn_simple = tf.keras.models.load_model("data/rnn/RNN_SIMPLE.h5")
     else:
         if embedding_matrix is None:
-            raise AttributeError(
-                "To load a non-pretrained model, an embedding_matrix is needed."
-            )
+            raise AttributeError("To load a non-pretrained model, an embedding_matrix is needed.")
 
-        model_rnn_simple = tf.keras.Sequential(
-            [
-                tf.keras.layers.Embedding(
-                    vocab_size,
-                    EMBEDDING_DIM,
-                    weights=[embedding_matrix],
-                    input_length=MAX_SEQUENCE_LENGTH,
-                    trainable=False,
-                ),
-                tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
-                tf.keras.layers.Dense(64, activation="relu"),
-                tf.keras.layers.Dense(1, activation="sigmoid"),
-            ]
-        )
+        model_rnn_simple = tf.keras.Sequential([
+            tf.keras.layers.Embedding(vocab_size,
+                                      EMBEDDING_DIM,
+                                      weights=[embedding_matrix],
+                                      input_length=MAX_SEQUENCE_LENGTH,
+                                      trainable=False),
+            tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
+            tf.keras.layers.Dense(64, activation="relu"),
+            tf.keras.layers.Dense(1, activation="sigmoid"),
+        ])
 
-        model_rnn_simple.compile(
-            loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-            optimizer=tf.keras.optimizers.Adam(1e-4),
-            metrics=["accuracy"],
-        )
+        model_rnn_simple.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                                 optimizer=tf.keras.optimizers.Adam(1e-4),
+                                 metrics=["accuracy"])
 
     return model_rnn_simple
 
 
-def get_clstm(
-    pretrained=False,
-    vocab_size=309467,
-    embedding_matrix=None,
-    EMBEDDING_DIM=200,
-    MAX_SEQUENCE_LENGTH=50,
-):
+def get_clstm(pretrained=False,
+              vocab_size=309467,
+              embedding_matrix=None,
+              EMBEDDING_DIM=200,
+              MAX_SEQUENCE_LENGTH=50):
+
     if pretrained:
         model_CLSTM = tf.keras.models.load_model("data/rnn/CLSTM.h5")
     else:
         if embedding_matrix is None:
-            raise AttributeError(
-                "To load a non-pretrained model, an embedding_matrix is needed."
-            )
+            raise AttributeError("To load a non-pretrained model, an embedding_matrix is needed.")
 
-        model_CLSTM = tf.keras.Sequential(
-            [
-                tf.keras.layers.Embedding(
-                    vocab_size,
-                    EMBEDDING_DIM,
-                    weights=[embedding_matrix],
-                    input_length=MAX_SEQUENCE_LENGTH,
-                    trainable=False,
-                ),
+        model_CLSTM = tf.keras.Sequential([
+                tf.keras.layers.Embedding(vocab_size,
+                                          EMBEDDING_DIM,
+                                          weights=[embedding_matrix],
+                                          input_length=MAX_SEQUENCE_LENGTH,
+                                          trainable=False),
                 tf.keras.layers.SpatialDropout1D(0.2),
                 tf.keras.layers.Conv1D(64, 5, activation="relu"),
-                tf.keras.layers.Bidirectional(
-                    tf.keras.layers.LSTM(64, dropout=0.2, recurrent_dropout=0.2)
-                ),
+                tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64,
+                                                                   dropout=0.2,
+                                                                   recurrent_dropout=0.2)),
                 tf.keras.layers.Dense(64, activation="relu"),
                 tf.keras.layers.Dropout(0.5),
                 tf.keras.layers.Dense(1, activation="sigmoid"),
-            ]
-        )
+        ])
 
-        model_CLSTM.compile(
-            optimizer=tf.keras.optimizers.Adam(lr=0.01),
-            loss="binary_crossentropy",
-            metrics=["accuracy"],
-        )
+        model_CLSTM.compile(optimizer=tf.keras.optimizers.Adam(lr=0.01),
+                            loss="binary_crossentropy",
+                            metrics=["accuracy"])
 
     return model_CLSTM
 
@@ -109,52 +91,53 @@ def get_gru_rnn(
         model_GRU = tf.keras.models.load_model("data/rnn/GRU.h5")
     else:
         if embedding_matrix is None:
-            raise AttributeError(
-                "To load a non-pretrained model, an embedding_matrix is needed."
-            )
+            raise AttributeError("To load a non-pretrained model, an embedding_matrix is needed.")
 
-        model_GRU = tf.keras.Sequential(
-            [
-                tf.keras.layers.Embedding(
-                    vocab_size,
-                    EMBEDDING_DIM,
-                    weights=[embedding_matrix],
-                    input_length=MAX_SEQUENCE_LENGTH,
-                    trainable=True,
-                ),
+        model_GRU = tf.keras.Sequential([
+                tf.keras.layers.Embedding(vocab_size,
+                                          EMBEDDING_DIM,
+                                          weights=[embedding_matrix],
+                                          input_length=MAX_SEQUENCE_LENGTH,
+                                          trainable=True),
                 tf.keras.layers.GRU(units=16, return_sequences=True),
                 tf.keras.layers.GRU(units=8, return_sequences=True),
                 tf.keras.layers.GRU(units=4),
                 tf.keras.layers.Dense(1, activation="sigmoid"),
-            ]
-        )
+        ])
 
         optimizer = tf.keras.optimizers.Adam(lr=1e-3)
-        model_GRU.compile(
-            loss="binary_crossentropy", optimizer=optimizer, metrics=["accuracy"]
-        )
+        model_GRU.compile(loss="binary_crossentropy",
+                          optimizer=optimizer,
+                          metrics=["accuracy"])
 
     return model_GRU
 
 
-def inference(sentences):
+def inference(sentences, pretrained=True):
     input_text = encode_text(sentences)
 
-    embedding_matrix = load_from_pickle("data/rnn/embedding_matrix.pkl")
+    if not pretrained:
+        embedding_matrix = load_from_pickle("data/rnn/embedding_matrix.pkl")
+    else:
+        embedding_matrix = None
 
-    biderectional = get_bidirectional_rnn(
-        pretrained=True, embedding_matrix=embedding_matrix
-    )
-    clstm = get_clstm(pretrained=True, embedding_matrix=embedding_matrix)
-    gru = get_gru_rnn(pretrained=True, embedding_matrix=embedding_matrix)
-
-    biderectional_output = biderectional.predict(input_text)
-    clstm_output = clstm.predict(input_text)
-    gru_ouput = gru.predict(input_text)
-
+    biderectional_model = get_bidirectional_rnn(pretrained=True, embedding_matrix=embedding_matrix)
+    biderectional_output = biderectional_model.predict(input_text)
     biderectional_output = interpret_results(biderectional_output, threshold=0)
+
+    del biderectional_model
+
+    clstm_model = get_clstm(pretrained=True, embedding_matrix=embedding_matrix)
+    clstm_output = clstm_model.predict(input_text)
     clstm_output = interpret_results(clstm_output, threshold=0.5)
+
+    del clstm_model
+
+    gru_model = get_gru_rnn(pretrained=True, embedding_matrix=embedding_matrix)
+    gru_ouput = gru_model.predict(input_text)
     gru_ouput = interpret_results(gru_ouput, threshold=0.5)
+
+    del gru_model
 
     return {
         "bidirectional": biderectional_output,
