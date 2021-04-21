36cbd168c1aa453b8f901cc8406e656a11febfba
Juan Barajas <jbperches@gmail.com>
2021-04-20 20:41:16 -0500

    Added cleanup after using voice recognition.

--- src/models/voice_recognition.py
+++ src/models/voice_recognition.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 import sounddevice as sd
@@ -8,14 +9,14 @@ from speechbrain.pretrained import TransformerASR
 
 def audio_to_string(duration=5, sample_rate=48000, voice_path='data/voice_model/'):
     voice_path = Path(voice_path)
-    print('Loading model...')
+    print('Loading voice recognition model...')
 
     asr_model = TransformerASR.from_hparams(
         source="speechbrain/asr-transformer-transformerlm-librispeech",
         savedir=str(voice_path),
     )
 
-    input('Ready to record audio. Press [ENTER] ')
+    input(f'Ready to record {duration} seconds of audio. Press [ENTER] ')
     selection = 'n'
 
     transcription = ''
@@ -27,14 +28,17 @@ def audio_to_string(duration=5, sample_rate=48000, voice_path='data/voice_model/
                            channels=2)
         sd.wait()
 
-        print('Now playing your recording:')
+        print('Playing your recording...')
         sd.play(recording, sample_rate)
         sf.write(str(voice_path/'recording.wav'), recording, sample_rate, format='WAV')
 
+        print('Performing transcription...')
         transcription = asr_model.transcribe_file(str(voice_path/'recording.wav'))
         print('The transcription: ', transcription)
 
         selection = input('Keep this result? [y/n]: ').lower()
+
+    os.remove('./recording.wav')
 
     return transcription
 
