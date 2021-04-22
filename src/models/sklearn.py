6db82c1c2082af9b00035c2720b606b58eabc8df
JohanCMT <48572205+JohanCMT@users.noreply.github.com>
2021-04-22 18:58:30 -0500

    Update sklearn.py

--- src/models/sklearn.py
+++ src/models/sklearn.py
@@ -35,22 +35,22 @@ def inference(lista):
   text_embbeded = [df_to_embedded_df(element,word_embedded,embeddings_phonetics) for element in lista]
   embbeded_mean= [np.mean(element,axis=0).reshape(1,-1) for element in text_embbeded]
 
-  BernoulliNB_classifier = load_pickle("drive/My Drive/Colab Notebooks/sentiment/Sklearn models/BernoulliNB_classifier_embedded")
+  BernoulliNB_classifier = load_pickle("data/sklearn/BernoulliNB_classifier_embedded")
   prediction = [BernoulliNB_classifier.predict(text) for text in embbeded_mean]
   prediction_dictionary["BernoulliNB"] = prediction
   del BernoulliNB_classifier
 
-  SGDClassifier_classifier = load_pickle("drive/My Drive/Colab Notebooks/sentiment/Sklearn models/SGDClassifier_classifier_embbeded")
+  SGDClassifier_classifier = load_pickle("data/sklearn/SGDClassifier_classifier_embbeded")
   prediction = [SGDClassifier_classifier.predict(text) for text in embbeded_mean]
   prediction_dictionary["SGDClassifier"] = prediction
   del SGDClassifier_classifier
 
-  LogisticRegression_classifier = load_pickle("drive/My Drive/Colab Notebooks/sentiment/Sklearn models/LogisticRegression_classifier_embbeded")
+  LogisticRegression_classifier = load_pickle("data/sklearn/LogisticRegression_classifier_embbeded")
   prediction = [LogisticRegression_classifier.predict(text) for text in embbeded_mean]
   prediction_dictionary["LogisticRegression"] = prediction
   del LogisticRegression_classifier
 
-  RandomForest_classifier = load_pickle("drive/My Drive/Colab Notebooks/sentiment/Sklearn models/SGDClassifier_classifier_embbeded")
+  RandomForest_classifier = load_pickle("data/sklearn/SGDClassifier_classifier_embbeded")
   prediction = [RandomForest_classifier.predict(text) for text in embbeded_mean]
   prediction_dictionary["RandomForest"] = prediction
   del RandomForest_classifier
