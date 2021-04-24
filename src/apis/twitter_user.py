0de83830971bfceeb095d3bba36104292705cea5
Juan Barajas <jbperches@gmail.com>
2021-04-24 00:40:58 -0500

    Formatted twitter api module

--- src/apis/twitter_user.py
+++ src/apis/twitter_user.py
@@ -1,33 +1,45 @@
-import sys, tweepy, pickle
-'''authentication function'''
+import os
+
+import tweepy
+import pickle
+
+
 def twitter_auth():
-    try: 
-        consumer_key =''
-        consumer_secret= ''
-        access_token=''
-        access_secret=''
-    except KeyError:
-        sys.stderr.write('TWITEER_* environment variable not set\n')
-        sys.exit(1)
-    auth= tweepy.OAuthHandler(consumer_key,consumer_secret)
+    consumer_key = os.environ.get('TWITTER_CONSUMER_KEY')
+    consumer_secret = os.environ.get('TWITTER_CONSUMER_SECRET')
+    access_token = os.environ.get('TWITTER_ACCESS_TOKEN')
+    access_secret = os.environ.get('TWITTER_ACCESS_SECRET')
+
+    if not all([consumer_key, consumer_secret, access_token, access_secret]):
+        raise AttributeError('TWITEER_* environment variable not set\n')
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_secret)
     return auth
-        
+
+
 def get_twitter_client():
-        auth= twitter_auth()
-        client= tweepy.API(auth, wait_on_rate_limit=True)
-        return client
-    
+    auth = twitter_auth()
+    client = tweepy.API(auth, wait_on_rate_limit=True)
+    return client
+
+
+def tweets_from_user(user=None):
+    if user is None:
+        user = input("Enter username: ")
+
+    client = get_twitter_client()
+    tweets = tweepy.Cursor(client.user_timeline, screen_name=user).items()
+
+    user_tweets = [status.text for status in tweets]
+    with open('user_tweet', 'wb') as fp:
+        pickle.dump(user_tweets, fp)
+    with open('user_tweet', 'rb') as fp:
+        user_tweets = pickle.load(fp)
+
+    return user_tweets
+
+
 if __name__ == '__main__':
-        user=input("Enter username: ")
-        client = get_twitter_client()
-        tweets= tweepy.Cursor(client.user_timeline, screen_name=user).items()
-       
-            
-        user_tweets=[status.text for status in tweets]
-        with open('user_tweet', 'wb') as fp:
-            pickle.dump(user_tweets, fp)    
-        with open ('user_tweet', 'rb') as fp:
-            user_tweets = pickle.load(fp)
-        print(user_tweets)
-            
+    print(tweets_from_user())
+
